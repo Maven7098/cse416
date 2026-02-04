69899a13918aa5d9f572cea8361b80a6d3c30408
Seongwoo Hong <seongwoo.hong@stonybrook.edu>
2026-02-04 18:30:00 -0500

    2 maps done with CSS tricks, but still need to work with GeoJSON

--- cse416_frontend/src/Map.jsx
+++ cse416_frontend/src/Map.jsx
@@ -1,15 +1,27 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 
-const Map = () => {
+const Map = ({geojsonData}) => {
   return (
-    <MapContainer center={[37.7749, -122.4194]} zoom={13} className="leaflet-container">
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+    // TODO: Load the GeoJSON for the districting map
+    // For Map 1 only: Map 2 does not have districting map
+    // I will use GA for the prototype, although this may or may not be carried over to the final product
+    <div className="leaflet-containerset">
+      <MapContainer center={[33.2478, -83.4411]} zoom={13} className="leaflet-container">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON data={geojsonData} />
+      </MapContainer>
+      <MapContainer center={[33.2478, -83.4411]} zoom={13} className="leaflet-container">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
+    </div>
   );
 };
 
