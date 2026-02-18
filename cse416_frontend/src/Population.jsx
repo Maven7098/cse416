ce1dac0a3d65d9ccce7216082a5d6d25e10817a2
Seongwoo Hong <seongwoo.hong@stonybrook.edu>
2026-02-18 14:55:57 -0500

    Flipped the chart to look horizontal, still not responsive

--- cse416_frontend/src/Population.jsx
+++ cse416_frontend/src/Population.jsx
@@ -23,30 +23,29 @@ function Population({districtData}){
     // Build a chart with D3 now
     // Set up margins and dimensions
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    const chartWidth = width - margin.left - margin.right;
-    const chartHeight = height - margin.top - margin.bottom;
+    const chartWidth = height - margin.top - margin.bottom;
+    const chartHeight = width - margin.left - margin.right;
     // Create scales
-    const x = d3
+    const x = d3.scaleLinear().domain([0, d3.max(data, (d) => d.value)]).range([0, chartHeight]);
+    const y = d3
         .scaleBand()
         .domain(data.map((d) => d.key))
-        .range([0, chartWidth])
+        .range([0, chartHeight])
         .padding(0.1);
-    const y = d3.scaleLinear().domain([0, d3.max(data, (d) => d.value)]).range([chartHeight, 0]);
 
     return(
         <div>
             <h1>District No.: {districtData.DISTRICT}</h1>
             <h3>District GeoID: {districtData.GEOID}</h3>
-            <svg width={width} height="auto">
+            <svg width={height} height={width}>
                 <g transform={`translate(${margin.left},${margin.top})`}>
                     {/* Bars */}
                     {data.map((d) => (
                     <rect
-                        key={d.key}
-                        x={x(d.key)}
-                        y={y(d.value)}
-                        width={x.bandwidth()}
-                        height={chartHeight - y(d.value)}
+                        x={x(0)}
+                        y={y(d.key)}
+                        width={x(d.value)}
+                        height={y.bandwidth()}
                         fill="steelblue"
                     />
                     ))}
