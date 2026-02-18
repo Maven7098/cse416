9b20fd572bd54040d00a42533d3f98c126eb6905
Seongwoo Hong <seongwoo.hong@stonybrook.edu>
2026-02-18 15:03:28 -0500

    Better looking barplot from https://www.react-graph-gallery.com/barplot

--- cse416_frontend/src/Population.jsx
+++ cse416_frontend/src/Population.jsx
@@ -1,9 +1,15 @@
-import { useEffect } from "react";
-import * as d3 from 'd3';
+import { useEffect, useMemo } from "react";
+import * as d3 from "d3";
 
-function Population({districtData}){
-    const width = 400; const height = 600;
-    
+const MARGIN = { top: 30, right: 30, bottom: 30, left: 30 };
+const BAR_PADDING = 0.3;
+
+export const Population = ({ districtData }) => {
+  // bounds = area inside the graph axis = calculated by substracting the margins
+  const width = 960; const height = 400;
+  const boundsWidth = width - MARGIN.right - MARGIN.left;
+  const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+
     useEffect(()=>{
         console.log(districtData)
     },[districtData])
@@ -19,47 +25,109 @@ function Population({districtData}){
     useEffect(()=>{
         console.log(data)
     },[data])
-    
-    // Build a chart with D3 now
-    // Set up margins and dimensions
-    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
-    const chartWidth = height - margin.top - margin.bottom;
-    const chartHeight = width - margin.left - margin.right;
-    // Create scales
-    const x = d3.scaleLinear().domain([0, d3.max(data, (d) => d.value)]).range([0, chartHeight]);
-    const y = d3
-        .scaleBand()
-        .domain(data.map((d) => d.key))
-        .range([0, chartHeight])
-        .padding(0.1);
 
-    return(
-        <div>
-            <h1>District No.: {districtData.DISTRICT}</h1>
-            <h3>District GeoID: {districtData.GEOID}</h3>
-            <svg width={height} height={width}>
-                <g transform={`translate(${margin.left},${margin.top})`}>
-                    {/* Bars */}
-                    {data.map((d) => (
-                    <rect
-                        x={x(0)}
-                        y={y(d.key)}
-                        width={x(d.value)}
-                        height={y.bandwidth()}
-                        fill="steelblue"
-                    />
-                    ))}
-                    {/* Axes */}
-                    <g
-                    className="x-axis"
-                    transform={`translate(0,${chartHeight})`}
-                    ref={(node) => d3.select(node).call(d3.axisBottom(x))}
-                    />
-                    <g className="y-axis" ref={(node) => d3.select(node).call(d3.axisLeft(y))} />
-                </g>
-            </svg>
-        </div>
+  // Y axis is for groups since the barplot is horizontal
+  const groups = data.map((d) => d.key);
+  const yScale = useMemo(() => {
+    return d3
+      .scaleBand()
+      .domain(groups)
+      .range([0, boundsHeight])
+      .padding(BAR_PADDING);
+  }, [data, height]);
+
+  // X axis
+  const xScale = useMemo(() => {
+    const [min, max] = d3.extent(data.map((d) => d.value));
+    return d3
+      .scaleLinear()
+      .domain([0, max || 10])
+      .range([0, boundsWidth]);
+  }, [data, width]);
+
+  // Build the shapes
+  const allShapes = data.map((d, i) => {
+    const y = yScale(d.key);
+    if (y === undefined) {
+      return null;
+    }
+
+    return (
+      <g key={i}>
+        <rect
+          x={xScale(0)}
+          y={yScale(d.key)}
+          width={xScale(d.value)}
+          height={yScale.bandwidth()}
+          opacity={0.7}
+          stroke="#9d174d"
+          fill="#9d174d"
+          fillOpacity={0.3}
+          strokeWidth={1}
+          rx={1}
+        />
+        <text
+          x={xScale(d.value) - 7}
+          y={y + yScale.bandwidth() / 2}
+          textAnchor="end"
+          alignmentBaseline="central"
+          fontSize={12}
+          opacity={xScale(d.value) > 90 ? 1 : 0} // hide label if bar is not wide enough
+        >
+          {d.value}
+        </text>
+        <text
+          x={xScale(0) + 7}
+          y={y + yScale.bandwidth() / 2}
+          textAnchor="start"
+          alignmentBaseline="central"
+          fontSize={12}
+        >
+          {d.key}
+        </text>
+      </g>
     );
-};
+  });
 
-export default Population;
+  const grid = xScale
+    .ticks(5)
+    .slice(1)
+    .map((value, i) => (
+      <g key={i}>
+        <line
+          x1={xScale(value)}
+          x2={xScale(value)}
+          y1={0}
+          y2={boundsHeight}
+          stroke="#808080"
+          opacity={0.2}
+        />
+        <text
+          x={xScale(value)}
+          y={boundsHeight + 10}
+          textAnchor="middle"
+          alignmentBaseline="central"
+          fontSize={9}
+          stroke="#808080"
+          opacity={0.8}
+        >
+          {value}
+        </text>
+      </g>
+    ));
+
+  return (
+    <div>
+      <svg width={width} height={height}>
+        <g
+          width={boundsWidth}
+          height={boundsHeight}
+          transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+        >
+          {grid}
+          {allShapes}
+        </g>
+      </svg>
+    </div>
+  );
+};
