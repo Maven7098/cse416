9fcc129e406b519b486e653e90737b2b1a4a8b62
Seongwoo Hong <seongwoo.hong@stonybrook.edu>
2026-05-02 12:21:02 -0400

    Modified TODO and generation script

--- pyei_trial/input1.sh
+++ pyei_trial/input1.sh
@@ -14,14 +14,14 @@ python3 presidential_ei.py IA-Precinct-EI.csv Hispanic Harris Complete/IA-Polari
 python3 presidential_ei.py IA-Precinct-EI.csv Hispanic Trump Complete/IA-Polarization-Hispanic-Trump
 python3 presidential_ei.py IA-Precinct-EI.csv Asian Harris Complete/IA-Polarization-Asian-Harris
 python3 presidential_ei.py IA-Precinct-EI.csv Asian Trump Complete/IA-Polarization-Asian-Trump
-python3 presidential_ei.py GA-Precinct-EI.csv White Harris Complete/GA-Polarization-White-Harris
-python3 presidential_ei.py GA-Precinct-EI.csv White Trump Complete/GA-Polarization-White-Trump
-python3 presidential_ei.py GA-Precinct-EI.csv Other Harris Complete/GA-Polarization-Other-Harris
-python3 presidential_ei.py GA-Precinct-EI.csv Other Trump Complete/GA-Polarization-Other-Trump
-python3 presidential_ei.py IA-Precinct-EI.csv White Harris Complete/IA-Polarization-White-Harris
-python3 presidential_ei.py IA-Precinct-EI.csv White Trump Complete/IA-Polarization-White-Trump
-python3 presidential_ei.py IA-Precinct-EI.csv Other Harris Complete/IA-Polarization-Other-Harris
-python3 presidential_ei.py IA-Precinct-EI.csv Other Trump Complete/IA-Polarization-Other-Trump
+#python3 presidential_ei.py GA-Precinct-EI.csv White Harris Complete/GA-Polarization-White-Harris
+#python3 presidential_ei.py GA-Precinct-EI.csv White Trump Complete/GA-Polarization-White-Trump
+#python3 presidential_ei.py GA-Precinct-EI.csv Other Harris Complete/GA-Polarization-Other-Harris
+#python3 presidential_ei.py GA-Precinct-EI.csv Other Trump Complete/GA-Polarization-Other-Trump
+#python3 presidential_ei.py IA-Precinct-EI.csv White Harris Complete/IA-Polarization-White-Harris
+#python3 presidential_ei.py IA-Precinct-EI.csv White Trump Complete/IA-Polarization-White-Trump
+#python3 presidential_ei.py IA-Precinct-EI.csv Other Harris Complete/IA-Polarization-Other-Harris
+#python3 presidential_ei.py IA-Precinct-EI.csv Other Trump Complete/IA-Polarization-Other-Trump
 
 deactivate
 
--- vra_code/TODO.sh
+++ vra_code/TODO.sh
@@ -4,12 +4,12 @@ source new-vra/bin/activate.fish
 
 mkdir Complete_256_Low Complete_4096_Low Complete_256_Medium Complete_4096_Medium Complete_256_High Complete_4096_High
 
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_High/NEW-GA-Precinct 14 256 0.75
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_Medium/NEW-GA-Precinct 14 256 0.6
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_Low/NEW-GA-Precinct 14 256 0.5
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_High/NEW-GA-Precinct 14 4096 0.75
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_Medium/NEW-GA-Precinct 14 4096 0.6
-#python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_Low/NEW-GA-Precinct 14 4096 0.5
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_High/NEW-GA-Precinct 14 256 0.75
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_Medium/NEW-GA-Precinct 14 256 0.6
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_256_Low/NEW-GA-Precinct 14 256 0.5
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_High/NEW-GA-Precinct 14 4096 0.75
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_Medium/NEW-GA-Precinct 14 4096 0.6
+python3 new_vra_parallel.py GA-Precinct-EI-GeoJSON.json Complete_4096_Low/NEW-GA-Precinct 14 4096 0.5
 
 python3 new_vra_parallel.py IA-Precinct-EI-GeoJSON.json Complete_256_High/NEW-IA-Precinct 4 256 0.75
 python3 new_vra_parallel.py IA-Precinct-EI-GeoJSON.json Complete_256_Medium/NEW-IA-Precinct 4 256 0.6
